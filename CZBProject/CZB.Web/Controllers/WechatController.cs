ffe4b99b962fbbddc62d6a750e6c29d11e548612
yuiane <yuiane@LAPTOP-2VI8E9AQ>
2017-08-08 15:39:47 +0800

    H5轮播案例

---
+++ CZBProject/CZB.Web/Controllers/WechatController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CZB.Web.Controllers
+{
+    public class WechatController : Controller
+    {
+        // GET: Wechat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Html5_One() {
+            return View();
+        }
+    }
+}
